460b76c8a0ad498a5e7ed8f1e82a98a0b73c007a
truyb <truyb@users.noreply.github.com>
2014-03-10 15:04:17 +0100

    Each level has a different colour, and the pictures work thanks to jQuery -B

--- olat2moodle.php
+++ olat2moodle.php
@@ -2,12 +2,15 @@
   /**********************************************************
   /* Stores the OLAT .zip that was uploaded previously, 
 	/* unzips it in a temporary folder with a random integer,
-	/* (so that there can't be any overwrites).
+	/* (so that there can't be any accidental overwrites).
 	/* Then it reads out runstructure.xml and gets out
 	/* the important parts and shows them on the page.
 	/**********************************************************
 	/* Bert Truyens
 	/*********************************************************/
+	
+	require_once("functions.php");
+	
 	if(isset($_FILES["file"])) {
 		if($_FILES["file"]) {
 		
@@ -27,12 +30,16 @@
 			
 			$path = $_FILES["file"]["tmp_name"];
 			
+			// Extracts the .zip and puts it in its own folder
+			// with randomly generated number for name.
 			$zip = new ZipArchive;
 			if ($zip->open($path)) {
 				$expath = getcwd() . "/tmp/" . $num . "/";
+				//$expath = getcwd() . "/tmp/";
 				$zip->extractTo($expath);
 				$zip->close();
 				
+				// $olat will be the root for the XML file.
 				if($olat = simplexml_load_file($expath . "runstructure.xml")) {}
 				else {
 					echo "<p>Error reading XML.</p><br>";
@@ -49,40 +56,60 @@
 		echo "<p>No file found, did you land on this page by accident?</p><br>";
 		echo "<a href='index.php'>Go back</a>";
 	}
+	
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>OLAT2Moodle</title>
 	<meta charset="utf-8">
+	<!--<link rel="stylesheet" type="text/css" href="css/reset.css">-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script type="text/javascript">
+		$(function() {
+			$('img').each(function() {
+				var $img = $(this);
+				var oldimgsrc = $img.attr('src');
+				
+				var newimgsrc = "/olat2moodle/tmp/<?php echo $num ?>/coursefolder/" + oldimgsrc;
+				
+				$img.attr('src', newimgsrc);
+			});
+		});
+	</script>
 </head>
 <body>
 	<h1><?php echo $olat->rootNode->longTitle; ?></h1>
 	<h2><?php echo $olat->rootNode->shortTitle; ?></h2>
 	<h3>Type of course: <?php echo $olat->rootNode->attributes(); ?></h3>
-	<?php foreach ($olat->rootNode->children->children() as $child) {
-		echo $child->getName() . "<br>";
-		if ($child->children->count() > 0) {
-			foreach ($child->children->children() as $child2) {
-				echo "> " . $child2->getName() . "<br>";
-				if ($child2->children->count() > 0) {
-					foreach ($child2->children->children() as $child3) {
-						echo ">> " . $child3->getName() . "<br>";
-						if ($child3->children->count() > 0) {
-							foreach ($child3->children->children() as $child4) {
-								echo ">>> " . $child4->getName() . "<br>";
+	<?php foreach ($olat->rootNode->children->children() as $child) { 				// Reads out all children, and the children's children. ?>
+		<div class="root">
+		<?php echo $child->getName() . "<br>"; ?>
+		</div>
+		<?php if ($child->children->count() > 0) {
+			foreach ($child->children->children() as $child2) { ?>
+				<div class="rootc1">
+				<?php echo "> " . $child2->getName() . "<br>";
+				echo $child2->shortTitle . "<br>"; ?>
+				</div>
+				<?php if ($child2->children->count() > 0) {
+					foreach ($child2->children->children() as $child3) { ?>
+						<div class="rootc2">
+						<?php echo ">> " . $child3->getName() . "<br>";
+						echo $child3->shortTitle . "<br>";
+						foreach ($child3->moduleConfiguration->config->entry as $strng) {
+							if($strng->string == "file") {
+								$url = $expath . "coursefolder" . $strng->string[1];
+								echo "<p>" . (file_get_contents($url)) . "</p>";
 							}
-						}
-					}
+						} ?>
+						</div>
+					<?php }
 				}
 			}
 		}
 		echo "<br>";
 	} ?>
-	<p><?php echo $olat->rootNode->children->$st->children->$st->children->$sp->longTitle; ?></p>
-	<?php foreach ($olat->rootNode->children->$st->children->$st->children->$sp->moduleConfiguration->config->entry as $strng) {
-		if($strng->string == "file") { ?>
-			<p><?php echo (file_get_contents($expath . "coursefolder" . $strng->string[1])); ?></p>
-		<?php } } ?>
 </body>
 </html>
