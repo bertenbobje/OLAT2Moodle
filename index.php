efc6799b8441f5f267e39be263c43856779c33b4
truyb <truyb@users.noreply.github.com>
2014-03-25 16:10:31 +0100

    #628
    - Added 'theme' and 'groupingid' to course.xml and every module.xml respectively to get rid of the warnings while restoring
    - .htm pages now also get caught instead of only .html pages.
    - Added a temporary PHP memory limit of -1 (no limit) to make sure the huge courses also get through.
    - &'s now get shown correctly in titles (they used to get cut off and PHP would show a warning)
    -- BOOKS
    - Written a function to check for more than two pages after eachother
    - When a collection of pages could become a book, their class gets updated with a $book equals true value, otherwise it stays false
    - Added a checkbox in the startpage to give the end user the choice to replace their pages with a book
    - Books still need implementation itself when making the Moodle backup, this is just the objects that get the value

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
 	<h1>OLAT2Moodle</h1>
 	<p>Welcome to OLAT2Moodle, please upload your OLAT export .zip file below.</p>
 	<form action="olat2moodle.php" method="post" enctype="multipart/form-data">
-		<label for="file">Filename:</label><br>
-		<input type="file" name="file" id="file"><br><br>
+		<label for="file">File:</label><br>
+		<input type="file" name="file" id="file">
+		<input type="checkbox" name="books" id="books">Turn pages into books?<br><br>
 		<input type="submit" name="submit" value="Submit">
 	</form>
 </body>
