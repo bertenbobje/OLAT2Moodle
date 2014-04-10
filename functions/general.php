496703c6bd4f778706794568c4c04763bd818ca5
truyb <truyb@users.noreply.github.com>
2014-04-10 12:09:27 +0200

    #628
    - Folders won't show up expanded anymore, since they won't appear in the navigation window if they are. -B

---
+++ functions/general.php
@@ -0,0 +1,95 @@
+<?php
+
+///////////////////////////////////////////////////////////
+// GENERAL FUNCTIONS //////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Creates an array to hold the directory list.
+// Checks for every file, including in subfolders.
+//
+// PARAMETERS
+// -> $dir = The directory
+function listFolderFiles($dir){
+	$result = array();
+	foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(realpath($dir), RecursiveDirectoryIterator::SKIP_DOTS)) as $filename) {
+		$result[] = substr($filename, strlen($dir) + 1);
+	}
+	return $result;
+}
+
+// Removes a folder with all its subfolders and files.
+// (Pretty much a 'rm -rf' command for PHP)
+//
+// PARAMETERS
+// -> $dir = The directory
+function rrmdir($dir) {
+	if (is_dir($dir)) {
+		$objects = scandir($dir);
+		foreach ($objects as $object) {
+			if ($object != "." && $object != "..") {
+				if (filetype($dir . "/" . $object) == "dir") {
+					rrmdir($dir . "/" . $object); 
+				}
+				else {
+					unlink($dir . "/" . $object);
+				}
+			}
+		}
+		reset($objects);
+		rmdir($dir);
+	}
+}
+
+// Here we read the archived file without unzipping it.
+// We run a check in the files under coursefolder to see if any duplicate file names exist
+// If so, we trigger a warning that also shows the file name in question.
+//
+// PARAMETERS
+// -> $zippedzip = The .zip file
+function checkDoubleFileReference($zippedzip) {
+	$courseFolderReached = false;
+	$i = 0;
+	for($i = 0; $i < $zippedzip->numFiles; $i++) {
+		$stat = $zippedzip->statIndex($i);
+		$zipfiles[$i] = basename($stat['name']);
+	}
+		
+	foreach ($zipfiles as $entry) {
+		if ($entry == "coursefolder") {
+			$courseFolderReached = true;
+		}
+		if ($entry == "editortreemodel.xml") {
+			$courseFolderReached = false;
+		}
+		if($courseFolderReached == true) {
+			$i++;
+			$dirdump[$i] = $entry;
+		}
+	}
+	
+	if (isset($dirdump)) {
+		$dirdump = array_map('strtolower', $dirdump);
+		$diff = array_count_values($dirdump);
+		foreach ($diff as $key=>$val) {
+			if ($val != 1) {
+				echo "<p style='color:red;'>WARNING - The coursefolder directory contains duplicate files, these will be discarded. 
+						Some content may not load in Moodle as a result. The discarded content is named: " . $key . "</p>";
+			}
+		}
+	}
+}
+
+// Cleans a string for any characters that could break a filename.
+//
+// PARAMETERS
+// -> $string = The string
+function clean($string) {
+	// Replaces all spaces with hyphens.
+	$string = str_replace(' ', '-', $string);
+	// Removes special chars.
+	$string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+	// Replaces multiple hyphens with single one.
+	return preg_replace('/-+/', '-', $string);
+}
+
+?>
---
+++ olat2moodle.php
@@ -0,0 +1,87 @@
+<?php
+
+/*************************************************************
+/* Converts an .OLAT backup to an object, converts
+/* this to a Moodle object, then this object gets used to
+/* write the backup files to restore into Moodle.
+/*************************************************************
+/* Bert Truyens
+/************************************************************/
+
+require_once("functions/olatBackupToOlatObject.php");
+require_once("functions/olatObjectToMoodleObject.php");
+require_once("functions/moodleObjectToMoodleBackup.php");
+
+// To make sure that every action can happen, even with bigger files.
+ini_set('max_execution_time', 300);
+ini_set('memory_limit', '-1');
+
+ini_set('xdebug.var_display_max_data', -1);
+ini_set('xdebug.var_display_max_children', -1);
+ini_set('xdebug.var_display_max_depth', -1);
+
+echo '<html>
+	<head>
+		<title>OLAT2Moodle</title>
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+	</head>
+	<body>
+';
+
+if(isset($_POST['books'])) {
+	if ($_POST['books'] == "on") {
+		$books = true;
+		$chapterFormat = $_POST['chaptertype'];
+	}
+}
+else {
+	$books = false;
+	$chapterFormat = "";
+}
+
+if (isset($_FILES["file"])) {
+	if (file_exists($_FILES["file"]["tmp_name"]) || is_uploaded_file($_FILES["file"]["tmp_name"])) {
+		// Creates an OLAT Object out of an exported OLAT course.
+		$olatObject = olatBackupToOlatObject($_FILES["file"]["tmp_name"]);
+		if ($olatObject !== null) {
+			echo "<br><p>===OLAT OBJECT===</p>";
+			//var_dump($olatObject);
+			echo "<p>OK - OLAT Object created</p><br>";
+			
+			echo "<p>===MOODLE OBJECT===</p>";
+			// Converts the OLAT Object to a Moodle object.
+			$moodleObject = olatObjectToMoodleObject($olatObject, $books);
+			//var_dump($moodleObject);
+			echo "<p>OK - Moodle Object created</p>";
+
+			if ($books) {
+				$moodleObject = checkForBooks($moodleObject);
+				//var_dump($moodleObject);
+				echo "<p style='color:green;'>OK - Books marked</p>";
+			}
+
+			$moodleObject = fixHTMLReferences($moodleObject, $olatObject, $books);
+			echo "<p>OK - All HTML references fixed</p>";
+
+			echo "<br><p>===MOODLE BACKUP===</p>";
+			// Uses the Moodle Object to make a Moodle backup .mbz file.
+			$moodleBackup = moodleObjectToMoodleBackup($moodleObject, $olatObject, $books, $chapterFormat);
+			echo "<p>OK - Moodle backup .mbz created</p><br>";
+
+			echo "<a href='" . dirname($_SERVER['PHP_SELF']) . $moodleBackup . "'>Download here</a>";
+		}
+	}
+	else {
+		echo "<p style='color:red;'>ERROR - No file found, did you land on this page by accident?</p><br><a href='index.php'>Go to start page</a>";
+	}
+}
+else {
+	echo "<p style='color:red;'>ERROR - No file found, did you land on this page by accident?</p><br><a href='index.php'>Go to start page</a>";
+}
+
+echo '
+	</body>
+</html>
+';
+
+?>
