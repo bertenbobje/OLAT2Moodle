d244e4fe18100c8453c02a0d2559fd682b9aca74
truyb <truyb@localhost>
2014-04-24 15:27:56 +0200

    #660
    - Even more stripping
    - Most functions put back in commentary, only the OLAT object parser is needed for now.

--- olat2moodle.php
+++ olat2moodle.php
@@ -50,12 +50,12 @@ if (isset($_FILES["file"])) {
 			
 			echo "<p>===MOODLE OBJECT===</p>";
 			// Converts the OLAT Object to a Moodle object.
-			$moodleObject = olatObjectToMoodleObject($olatObject, $books);
+			//$moodleObject = olatObjectToMoodleObject($olatObject, $books);
 			//var_dump($moodleObject);
 			echo "<p>OK - Moodle Object created</p>";
 
 			if ($books) {
-				$moodleObject = checkForBooks($moodleObject);
+				//$moodleObject = checkForBooks($moodleObject);
 				//var_dump($moodleObject);
 				echo "<p style='color:green;'>OK - Books marked</p>";
 			}
@@ -65,10 +65,10 @@ if (isset($_FILES["file"])) {
 
 			echo "<br><p>===MOODLE BACKUP===</p>";
 			// Uses the Moodle Object to make a Moodle backup .mbz file.
-			$moodleBackup = moodleObjectToMoodleBackup($moodleObject, $olatObject, $books, $chapterFormat);
+			//$moodleBackup = moodleObjectToMoodleBackup($moodleObject, $olatObject, $books, $chapterFormat);
 			echo "<p>OK - Moodle backup .mbz created</p><br>";
 
-			echo "<a href='" . dirname($_SERVER['PHP_SELF']) . $moodleBackup . "'>Download here</a>";
+			//echo "<a href='" . dirname($_SERVER['PHP_SELF']) . $moodleBackup . "'>Download here</a>";
 		}
 	}
 	else {
